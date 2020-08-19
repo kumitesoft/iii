36a6f237bf6b02cb9215db5cc3ce3123733b7df0
Marczeli <aleczas1@onet.pl>
2020-08-19 20:17:35 +0200

    1

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.get('/', function(req, res) {
 });
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('../client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 app.listen(port, function() {
   console.log('Server is running on port: ' + port);
